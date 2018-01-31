ed73ba4c435ac5f0da25df1845b3da52caf01f85
muzakparov <muzakparov@gmail.com>
2018-01-31 11:01:54 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import NavigationList from './components/NavigationList';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavigationList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavigationList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavigationList extends Component {
+  render() {
+    return (
+      <div>
+        <div>NavigationList</div>
+
+        
+      </div>
+    );
+  }
+}
+
+export default NavigationList;
